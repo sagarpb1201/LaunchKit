7e06f2ad395f34367c0c19c57a0e3e58653b4941
Sagar <sagarpb1201@gmail.com>
2025-10-25 15:25:42 +0530

    refactor(backend): improve signup error handling and remove logs

--- backend/src/api/v1/controllers/user.controller.ts
+++ backend/src/api/v1/controllers/user.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import * as userService from '../services/user.service';
+import { ZodError } from 'zod';
 import { createUserSchema } from '../validators/user.validator';
+import z from 'zod';
 
 export const getAllUsers = async (req: Request, res: Response) => {
   try {
@@ -13,19 +15,23 @@ export const getAllUsers = async (req: Request, res: Response) => {
 
 export const signupUser = async (req: Request, res: Response) => {
   try {
-    console.log("req",req.body)
     const { name, email, password } = createUserSchema.parse(req).body;
 
     const user = await userService.createUser({ name, email, password });
 
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { password: _, ...userWithoutPassword } = user;
 
     res.status(201).json({ success: true, data: userWithoutPassword });
   } catch (error: any) {
-    console.log("error signup",error)
+    if (error instanceof ZodError) {
+        const formattedErrors=z.treeifyError(error)
+      return res.status(400).json({ success: false, errors:formattedErrors});
+    }
     if (error.message.includes('already exists')) {
       return res.status(409).json({ success: false, error: error.message });
     }
-    res.status(500).json({ success: false, error: 'An error occurred during signup.' });
+    console.error("Error during signup:", error);
+    res.status(500).json({ success: false, error: 'An unexpected error occurred during signup.' });
   }
 };
